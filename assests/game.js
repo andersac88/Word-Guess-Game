1ef594839b8e7969b679ba51f76214d2b2a4df9e
Andrew Andersen <andersac88@gmail.com>
2018-09-17 18:34:15 -0500

    dashes

--- assests/game.js
+++ assests/game.js
@@ -31,41 +31,42 @@ var userChoiceText = document.getElementById("userchoice-text");
 var computerChoiceText = document.getElementById("computerchoice-text");
 var winsText = document.getElementById("wins-text")
 var guessesRemainingText = document.getElementById("guessesremaining-text")
+var dashesChoiceText = document.getElementById("dashes")
 
 // create a function where computer selects a random word
 
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var dashes = computerGuess.replace(/[a-z]/g,"-");
 
 // Create Function to start game - press any key to start
 document.onkeyup = function(event) {
+    
+    var userGuess = event.key;
 
     if (guessesRemaining === 0) {
         console.log("LOSER");
         return;
     }
-
-    var userGuess = event.key;
-   
-    
+     
     for (var i = 0; i < computerGuess.length; i++) {
         console.log(computerGuess[i]);
-        if (computerGuess[i] === userGuess) {
+       <---- if (computerGuess[i] === userGuess) {
+            dashes.replace(/[i]/g,userGuess);
             console.log("success");
         }     
-    } if ((computerGuess[0] !== userGuess) && 
-         (computerGuess[1] !== userGuess) &&
-         (computerGuess[2] !== userGuess) &&
-         (computerGuess[3] !== userGuess) &&
-         (computerGuess[4] !== userGuess) &&
-         (computerGuess[5] !== userGuess) &&
-         (computerGuess[6] !== userGuess) &&
-         (computerGuess[7] !== userGuess) &&
-         (computerGuess[8] !== userGuess) &&
-         (computerGuess[9] !== userGuess)){
+    } if ((computerGuess[0] !== userGuess) &&
+        (computerGuess[1] !== userGuess) &&
+        (computerGuess[2] !== userGuess) &&
+        (computerGuess[3] !== userGuess) &&
+        (computerGuess[4] !== userGuess) &&
+        (computerGuess[5] !== userGuess) &&
+        (computerGuess[6] !== userGuess) &&
+        (computerGuess[7] !== userGuess) &&
+        (computerGuess[8] !== userGuess) &&
+        (computerGuess[9] !== userGuess)){
         guessesRemaining--
     }
-
-
+    
 
 userChoices.push(" " + userGuess);
 
@@ -74,6 +75,7 @@ userChoiceText.textContent = "Incorrect Guesses: " + userChoices;
 userGuessText.textContent = "Most Recent Guess: " + userGuess;
 guessesRemainingText.textContent = guessesRemaining;
 computerChoiceText.textContent = computerGuess;
+dashesChoiceText.textContent = dashes;
 
 
 
@@ -94,3 +96,4 @@ computerChoiceText.textContent = computerGuess;
 //userChoiceText.appendChild(nextChoiceText);
 //nextChoiceText.textContent = userGuess;
 //var nextChoiceText = document.createElement("span")
+
