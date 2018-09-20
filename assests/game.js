d8866e0e2ef546b9a50cb033a082570a3abd8657
Andrew Andersen <andersac88@gmail.com>
2018-09-19 23:38:53 -0500

    Add files via upload

--- assests/game.js
+++ assests/game.js
@@ -1,20 +1,16 @@
 var wins = 0
 var guessesRemaining = 10
-
 var computerChoices = [
     "castle",
     "knight",
-    "queen",
     "king",
     "prince",
-    "dungeon",
-    "princess",
-    "jester",
+    "horse",
+    "lance",
     "sword",
     "plague",
     "camelot",
     "crusade",
-    "trebuchet",
     "joust",
 ];
 
@@ -28,44 +24,76 @@ var winsText = document.getElementById("wins-text")
 var guessesRemainingText = document.getElementById("guessesremaining-text")
 var dashesChoiceText = document.getElementById("dashes")
 
+
+
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-var dashes = computerGuess.replace(/[a-z]/g,"-");
+var dashGuess = [];
+
+for (var i=0; i < computerGuess.length; i++) {
+    dashGuess.push("_");
+    console.log(dashGuess);
+}
+
+
+// have key strokes start function
+    document.onkeyup = function(event) {
+        if(event.keyCode >= 65 && event.keyCode <= 90) {
+        var userGuess = (event.key.toLowerCase());
+        console.log(userGuess)
+        
+
+//Search computer selected word/guess for user Guess/keystroke; incorrect guess
+    console.log(computerGuess.search(userGuess));
+    if (computerGuess.search(userGuess) === -1) {
+        guessesRemaining--
+        userChoices.push(" " + userGuess)
+    } 
+//Search computer selected word/guess for user guess/keystroke; match
+    else if (computerGuess.search(userGuess) === 0) {
+        dashGuess[0] = userGuess
+    }   else if (computerGuess.search(userGuess) === 1) {
+        dashGuess[1] = userGuess
+    }   else if (computerGuess.search(userGuess) === 2) {
+        dashGuess[2] = userGuess
+    }   else if (computerGuess.search(userGuess) === 3) {
+        dashGuess[3] = userGuess
+    }   else if (computerGuess.search(userGuess) === 4) {
+        dashGuess[4] = userGuess
+    }   else if (computerGuess.search(userGuess) === 5) {
+        dashGuess[5] = userGuess
+    }   else if (computerGuess.search(userGuess) === 6) {
+        dashGuess[6] = userGuess
+    }   else if (computerGuess.search(userGuess) === 7) {
+        dashGuess[7] = userGuess
+    }   else if (computerGuess.search(userGuess) === 8) {
+        dashGuess[8] = userGuess
+    }   else if (computerGuess.search(userGuess) === 9) {
+        dashGuess[9] = userGuess
+    } 
+}
+
+//Restart Game when out of guesses
+    if (guessesRemaining === 0) {
+        document.getElementById("computerchoice-text").style.cssText = "visibility: visible";
+        var confirmRestart = confirm("Game Over. Would you like to try again?")
+        if(confirmRestart) {
+            window.location.reload();
+        }
+    }
+//Declare Victory        
+if(dashGuess.indexOf("_") === -1) {
+    wins++;
+// reset game?
+     }
 
 directionsText.textContent = "";
 userChoiceText.textContent = "Incorrect Guesses: " + userChoices;
 userGuessText.textContent = "Most Recent Guess: " + userGuess;
-guessesRemainingText.textContent = guessesRemaining;
+guessesRemainingText.textContent = "Guesses Remaining: " + guessesRemaining;
 computerChoiceText.textContent = computerGuess;
-dashesChoiceText.textContent = dashes;
-
-
-    document.onkeyup = function(event) {
-    var userGuess = event.key;
-
-    function evaluateGuess(letter) {
-        // Array to store positions of letters in string
-        var positions = [];
-    
-        // Loop through word finding all instances of guessed letter, store the indicies in an array.
-        for (var i = 0; i < computerChoices[computerGuess].length; i++) {
-            if(computerChoices[computerGuess][i] === userGuess) {
-                positions.push(i);
-            }
-        }
-    
-        // if there are no indicies, remove a guess and update the hangman image
-        if (positions.length <= 0) {
-            remainingGuesses--;
-        } else {
-            // Loop through all the indicies and replace the '_' with a letter.
-            for(var i = 0; i < positions.length; i++) {
-                dashes[positions[i]] = userGuess;
-            }
-        });
-     
-   
-
-
+dashesChoiceText.textContent = dashGuess;
+winsText.textContent = "Number of Wins: " + wins;
+};
 
 // notes to self......
 // have selection displayed where - - - - - - - represents letters of word
@@ -80,13 +108,5 @@ dashesChoiceText.textContent = dashes;
 //var nextChoiceText = document.createElement("span")
 
 //var txt2 = txt1.slice(0, 3) + "bar" + txt1.slice(3);
-//else if (computerGuess[1] === userGuess) {
-    //dashes = dashes.slice(0,1) + userGuess;
-   // else if (computerGuess[1] === userGuess) {
-    // dashes = dashes.slice(0,1) + userGuess;
-    // else if (computerGuess[1] === userGuess) {
-        /// dashes = dashes.slice(0,1) + userGuess;
-
-
 
 
