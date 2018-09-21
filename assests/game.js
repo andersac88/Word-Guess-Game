ea10462a0d2f005dbd033b03eb00d371667dd2d3
Andrew Andersen <andersac88@gmail.com>
2018-09-20 23:13:53 -0500

    tinkering

--- assests/game.js
+++ assests/game.js
@@ -1,4 +1,5 @@
 var wins = 0
+var losses = 0
 var guessesRemaining = 10
 var computerChoices = [
     "castle",
@@ -23,11 +24,10 @@ var directionsText = document.getElementById("directions-text");
 var userGuessText = document.getElementById("userguess-text");
 var userChoiceText = document.getElementById("userchoice-text");
 var computerChoiceText = document.getElementById("computerchoice-text");
-var winsText = document.getElementById("wins-text")
-var guessesRemainingText = document.getElementById("guessesremaining-text")
-var dashesChoiceText = document.getElementById("dashes")
-
-
+var winsText = document.getElementById("wins-text");
+var guessesRemainingText = document.getElementById("guessesremaining-text");
+var dashesChoiceText = document.getElementById("dashes");
+var lossText = document.getElementById("loss-text");
 
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 var dashGuess = [];
@@ -41,11 +41,21 @@ for (var i=0; i < computerGuess.length; i++) {
 }
 
 // define function to reset the game when you win
-function reset() {
+function winReset() {
      dashGuess = [];
      userChoices = [];
      computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
      guessesRemaining = 10;
+}
+
+// define a function to reset the game when you lose
+function lossReset() {
+    dashGuess = [];
+    document.getElementById("computerchoice-text").style.cssText = "visibility: hidden";
+    wins = 0;
+    userChoices = [];
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    guessesRemaining = 10;
 }
 
 //Run Function to start the game
@@ -57,7 +67,6 @@ begin();
         var userGuess = (event.key.toLowerCase());
         console.log(userGuess)
         
-
 //Search computer selected word/guess for user Guess/keystroke; incorrect guess
     console.log(computerGuess.search(userGuess));
     if (computerGuess.search(userGuess) === -1) {
@@ -74,25 +83,24 @@ for (var i = 0; i < computerGuess.length; i++) {
 
 //Restart Game when out of guesses
     if (guessesRemaining === 0) {
-        document.getElementById("computerchoice-text").style.cssText = "visibility: visible";
-        var confirmRestart = confirm("Game Over. Would you like to try again?")
-        if(confirmRestart) {
-            window.location.reload();
-        }
-    }
+        //document.getElementById("computerchoice-text").style.cssText = "visibility: visible";
+       lossReset()
+       begin()
+    }    
 //Declare Victory        
 if(dashGuess.indexOf("_") === -1) {
     wins++;
-    reset();
+    winReset();
     begin();
      }
 
+dashesChoiceText.textContent = dashGuess.join("");
 directionsText.textContent = "";
 userChoiceText.textContent = "Incorrect Guesses: " + userChoices;
 userGuessText.textContent = "Most Recent Guess: " + userGuess;
 guessesRemainingText.textContent = "Guesses Remaining: " + guessesRemaining;
 computerChoiceText.textContent = "Answer: " + computerGuess;
-dashesChoiceText.textContent = dashGuess.join("");
+lossText.textContent = "Number of Losses: " + losses;
 winsText.textContent = "Number of Wins: " + wins;
 }
     };
